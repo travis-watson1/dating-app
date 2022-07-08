a4521c595917cd50adb38580c57b630dabd0cea3
Travis Watson <watsontravis@protonmail.com>
2022-07-08 07:17:52 -0500

    Add EntityFramework and AppUser entity

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,13 +35,12 @@ namespace API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //This is where you would make services available to the rest of the application.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
 
             app.UseHttpsRedirection();
