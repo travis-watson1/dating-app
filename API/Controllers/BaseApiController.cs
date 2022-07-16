5053e524d4cb244bcffcada37869b94dc88f9320
Travis Watson <watsontravis@protonmail.com>
2022-07-15 22:22:21 -0500

    Add DTO for AccountController

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -8,7 +8,7 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class BaseApiController
+    public class BaseApiController : ControllerBase
     {
         
     }
