cfc63443e40c9d3dfccccf2fb4dbd1f3c804752d
Travis Watson <watsontravis@protonmail.com>
2023-03-10 21:32:21 -0600

    Add home component

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,17 +1,38 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../_models/user';
+import { map } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
 
 //Injectable means this service can be injected into other components or services in application
 @Injectable({
-  providedIn: 'root' //Services are singleton. Data doesn't get destroyed until application shuts down.Components are shut down when they are not in use.
+  providedIn: 'root', //Services are singleton. Data doesn't get destroyed until application shuts down.Components are shut down when they are not in use.
 })
 export class AccountService {
-
   baseUrl = 'https://localhost:5001/api/';
+  private currentUserSource = new BehaviorSubject<User | null>(null);
+  currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  login(model: any){
-    return this.http.post(this.baseUrl + 'account/login', model);
+  login(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
   }
 }
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,25 +1,38 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'The Dating App';
   users: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit() {
-    this.getUsers()
+    this.getUsers();
+    this.setCurrentUser();
   }
 
   getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error)
-    })
+      next: (response) => (this.users = response),
+      error: (error) => console.log(error),
+    });
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
 }
