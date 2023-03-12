a545de84041b1f321c79d85c7924b1ac31ae2ea4
Travis Watson <watsontravis@protonmail.com>
2023-03-12 06:24:44 -0500

    Add routing for login and logout

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,27 +12,19 @@ import { User } from '../_models/user';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService) {}
+  constructor(public accountService: AccountService, private router: Router) {}
 
   ngOnInit(): void {}
 
-  // getCurrentUser() {
-  //   this.accountService.currentUser$.subscribe({
-  //     next: (user) => (this.loggedIn = !!user),
-  //     error: (error) => console.log(error),
-  //   });
-  // }
-
   login() {
     this.accountService.login(this.model).subscribe({
-      next: (response) => {
-        console.log(response);
-      },
+      next: (_) => this.router.navigateByUrl('/members'),
       error: (error) => console.log(error),
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
