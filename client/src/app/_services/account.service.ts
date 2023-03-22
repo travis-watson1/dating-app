6874313bab12bd070bd18fe5cd4db3e4600f4400
Travis Watson <watsontravis@protonmail.com>
2023-03-21 20:51:44 -0500

    Add animated buttons

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { User } from '../_models/user';
 import { map } from 'rxjs/operators';
 import { BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 //Injectable means this service can be injected into other components or services in application
 @Injectable({
   providedIn: 'root', //Services are singleton. Data doesn't get destroyed until application shuts down.Components are shut down when they are not in use.
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   private currentUserSource = new BehaviorSubject<User | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
 
